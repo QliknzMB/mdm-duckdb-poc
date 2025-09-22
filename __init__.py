a50440b3381701b865f72b43d2e730292fd7253b
QliknzMB <marnus.bosch@gmail.com>
2025-09-22 15:32:51 +1200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+import logging
+import duckdb
+import os
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    # ... (the full Python code from our last attempt) ...
